d57ae3a347c85770d751de7af190e260447712d1
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-04-09 12:51:41 +0500

    localStorage implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddContact from "./AddContact";
 import "./App.css";
 import ContactList from "./ContactList";
 import Header from "./Header";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
     console.log(contact);
   };
+
+  useEffect(() => {
+    const retrievedContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    );
+    if (retrievedContacts) setContacts(retrievedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
